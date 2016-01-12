0a82638df05855ced5d7f7710f854b630a1138e5
Kaijian Gao <kgao@kayak.com>
2016-01-12 01:37:49 -0500

    Added new v2 site while preserving previous version. Photography page work in progress

--- server.js
+++ server.js
@@ -2,18 +2,82 @@ var util = require('util'),
     express = require('express'),
     expressValidator = require('express-validator'),
     app = express(),
-    email = require("./node_modules/emailjs/email");
+    email = require("./node_modules/emailjs/email"),
+    lessMiddleware = require('less-middleware'),
+    mustache = require('mustache'),
+    fs = require('fs'),
+    path = require('path'),
+    ExifImage = require('exif').ExifImage;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
+app.use(lessMiddleware(__dirname + '/public', {force: true}));
 app.use(express.static(__dirname + '/public'));
 
-app.post('/message', function(req, res){
+var engines = require('consolidate');
+app.engine('html', engines.mustache);
+app.set('views', __dirname + '/views')
+app.set('view engine', 'html');
 
-	
+app.get('/v2', function(req, res) {
+    console.log("sending html");
+    var files = (fs.readdirSync(__dirname + '/public/images/gallery'));
+
+    var images = Array();
+    for (var i in files) {
+        if(path.extname(files[i]) === ".jpg") {
+            images.push(files[i]);
+        }
+    }
+
+    var responseJson = Array();
+
+    for (var i = 0; i < images.length; i++) {
+        var imgName = images[i];
+        getImageExif(imgName, function(exifData, name) {
+            //console.log("imgpath: " + imgPath);
+            responseJson.push({
+                path: '/images/gallery/' + name,
+                year: exifData.exif.CreateDate.substr(0, 4),
+                name: name.split('.jpg')[0],
+                exif: exifData.exif
+            });
+
+            if (responseJson.length >= images.length) {
+                responseJson = responseJson.sort(alphabeticalCompare);
+                console.log(responseJson);
+                res.render('index', { images: responseJson});
+            }
+        });
+    }
+});
+
+function alphabeticalCompare(a,b) {
+    if (a.name < b.name)
+        return -1;
+    else {
+        return 1;
+    }
+}
+
+function getImageExif(imgName, callback) {
+    try {
+        new ExifImage({ image : __dirname + '/public/images/gallery/' + imgName }, function (error, exifData) {
+            if (error) {
+                console.log('Image: ' + imgName + ' - Error: '+error.message);
+            } else {
+                callback(exifData, imgName);
+            }
+        });
+    } catch (error) {
+        console.log('Error: ' + error.message);
+    }
+}
+
+app.post('/message', function(req, res){
 
 	req.sanitize('name').xss();
 	req.sanitize('email').xss();
@@ -41,33 +105,33 @@ app.post('/message', function(req, res){
 
 	// send email
 	var server  = email.server.connect({
-	   user:    "contact@kaijiangao.com", 
-	   password:"", 
-	   host:    "smtpout.secureserver.net", 
+	   user:    "contact@kaijiangao.com",
+	   password:"",
+	   host:    "smtpout.secureserver.net",
 	   ssl:     true
 
 	});
 
 	// send the message and get a callback with an error or details of the message that was sent
 	server.send({
-	   text:    message_content, 
-	   from:    name+" <"+email_address+">", 
+	   text:    message_content,
+	   from:    name+" <"+email_address+">",
 	   to:      "Kaijian Gao <kaijian_gao@brown.edu>",
 	   cc:      "Website <contact@kaijiangao.com>",
 	   subject: "Message from website"
-	}, function(err, message) { 
+	}, function(err, message) {
 		// send a copy of the message if copy is requested
 		if(copy != undefined && copy == 'on'){
 			server.send({
-			   text:    "This email is a confirmation that you have sent the following message to Kaijian Gao: \n" + message_content, 
-			   from:    "Kaijian Gao <contact@kaijiangao.com>", 
+			   text:    "This email is a confirmation that you have sent the following message to Kaijian Gao: \n" + message_content,
+			   from:    "Kaijian Gao <contact@kaijiangao.com>",
 			   to:      name+" <"+email_address+">",
 			   cc:      "",
 			   subject: "Confirmation for the message you sent to Gao"
 			}, function(err, message) { console.log(err || message); });
 		}
 
-		console.log(err || message); 
+		console.log(err || message);
 		if(err){
 			console.log('=====Error message!!=====: '+err);
 			res.send('Failed to send message because of error: '+err, 500);
@@ -78,11 +142,6 @@ app.post('/message', function(req, res){
 			});
 		}
 	});
-
-	
-
-
-
 });
 
 app.listen(3000, '', '', function(){
