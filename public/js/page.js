ad9f7c2975c07f1bc1ce5fdb9294d10982fae5a4
Kaijian Gao <kgao@kayak.com>
2016-01-19 23:53:58 -0500

    Added click-scroll transition. Disabled normal scrolling

--- public/js/page.js
+++ public/js/page.js
@@ -1,14 +1,7 @@
-var dim = function(element, amount) {
-    amount = 100 - amount;
-    $(element).css({
-        filter: "brightness(" + amount + "%)",
-        "-webkit-filter": "brightness(" + amount + "%)", /* Prefixed CSS3 blur filters */
-        "-o-filter": "brightness(" + amount + "%)",
-        "-ms-filter": "brightness(" + amount + "%)"
-    });
-}
+var currentPhoto = 0;
 
 $(document).ready(function() {
+    var photoCount = $(".photo").length;
 
     if (!window.requestAnimationFrame) {
         window.requestAnimationFrame = ( function() {
@@ -26,24 +19,77 @@ $(document).ready(function() {
         } )();
     }
 
-    window.onscroll= function() {
-        window.requestAnimationFrame(function () {
-            var scrollLeft = $(window).scrollLeft();
-            var windowWidth = window.innerWidth;
+    //window.onscroll= function() {
+    //    window.requestAnimationFrame(function () {
+    //        var scrollLeft = $(window).scrollLeft();
+    //        var windowWidth = window.innerWidth;
+    //
+    //        $(".galleryItem").each(function(idx, ele) {
+    //            var item = $(ele);
+    //            if (item.position().left < scrollLeft + windowWidth * 0.5) {
+    //                dim(item, 0);
+    //            } else {
+    //                var overScrollAmount = (item.position().left) - (scrollLeft + windowWidth * 0.5);
+    //                var brightness = overScrollAmount / (windowWidth * 0.5);
+    //                dim(item, brightness * 100);
+    //            }
+    //        });
+    //        console.log()
+    //    });
+    //};
 
-            $(".galleryItem").each(function(idx, ele) {
-                var item = $(ele);
-                if (item.position().left < scrollLeft + windowWidth * 0.5) {
-                    dim(item, 0);
-                } else {
-                    var overScrollAmount = (item.position().left) - (scrollLeft + windowWidth * 0.5);
-                    var brightness = overScrollAmount / (windowWidth * 0.5);
-                    dim(item, brightness * 100);
-                }
-            });
-            console.log()
-        });
-    };
+    $("#prevPhoto").click(function(){
+        if (currentPhoto > 0) {
+            currentPhoto--;
+        }
+        scrollToPic(currentPhoto);
+    });
+
+    $("#nextPhoto").click(function(){
+        if (currentPhoto < photoCount - 1) {
+            currentPhoto++;
+        }
+        scrollToPic(currentPhoto);
+    });
 
     $(window).trigger("scroll");
 });
+
+function scrollToPic(index) {
+    var leftPosition = 0;
+
+    if (index != 0) {
+        var photo = $(".photo").eq(index);
+        var padding = (photo.innerWidth() - photo.width()) / 2;
+
+        leftPosition = photo.position().left + padding - 50;
+
+        var galleryWidth = 0;
+        $(".galleryItem").each(function(idx, ele) {
+            galleryWidth += $(ele).width();
+        });
+        leftPosition = Math.min(leftPosition, galleryWidth - $(window).width());
+    }
+
+    allTransform($("#gallery"), "translate3d(-" + leftPosition + "px, 0, 0)");
+}
+
+function allTransform(element, value) {
+    $(element).css({
+        "-webkit-transform": value,
+        "-moz-transform":    value,
+        "-ms-transform":     value,
+        "-o-transform":      value,
+        "transform":         value
+    });
+}
+
+function dim(element, amount) {
+    amount = 100 - amount;
+    $(element).css({
+        filter: "brightness(" + amount + "%)",
+        "-webkit-filter": "brightness(" + amount + "%)", /* Prefixed CSS3 blur filters */
+        "-o-filter": "brightness(" + amount + "%)",
+        "-ms-filter": "brightness(" + amount + "%)"
+    });
+}
